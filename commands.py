5be2d5dcd5433b8694640717bb99505072046b1d
VatukkaVB <68278352+VatukkaVB@users.noreply.github.com>
2022-10-28 00:45:22 +0300

    chore: Tinker

--- commands.py
+++ commands.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
 
-CREATING_TEAM, LISTING_MEMBERS, HELP, CHOOSING, SHUFFLING, = range(5)
+CREATING_TEAM, ADDING_MEMBERS, LISTING_MEMBERS, HELP, CHOOSING, SHUFFLING, CHOOSING_TO_DO, = range(7)
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -10,7 +10,7 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id=update.effective_chat.id,
         text="Помощь"
     )
-    return CREATING_TEAM
+    return CHOOSING
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -24,31 +24,59 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
                   'Если хочешь создать новую группу, напиши или выбери кнопку "Создать группу". '
                   'Если хочешь получить список людей в группе, напиши или выбери кнопку "Кто в группе". '
                   )
+    if context.user_data:
+        del context.user_data
+    text = update.message.text.lower()
+    context.user_data['choice'] = text
     await update.message.reply_text(reply_text, reply_markup=markup)
     return CHOOSING
 
 
 async def create_team(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    team_members = update.message.parse_entities()
-    context.user_data['team_members'] = team_members
-    reply_text = "Введи список тегов тех, кого хочешь добавить к группе, через запятую:"
-    await update.message.reply_text(reply_text)
-    # context.bot.send_message(
-    #     text="Введи список тегов тех, кого хочешь добавить к группе, через запятую:",
-    #     chat_id=
-    # )
+    # reply_text = "Введи список тегов тех, кого хочешь добавить к группе, через запятую:"
+    # await update.message.reply_text(reply_text)
+    await context.bot.send_message(
+        text="Введи список тегов тех, кого хочешь добавить к группе, через запятую:",
+        chat_id=update.effective_message.chat_id
+    )
 
-    return LISTING_MEMBERS
+    return ADDING_MEMBERS
+
+
+async def add_members(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    team_members = update.message.parse_entities(types=["user"])
+    print(update.message.parse_entities(types=["user"]))
+    context.user_data['team_members'] = team_members
+    print("context: ", context.user_data)
+    if context.user_data['team_members']:
+        return LISTING_MEMBERS
+    else:
+        await context.bot.send_message(
+            text="Что-то пошло не так!",
+            chat_id=update.effective_message.chat_id
+        )
+        return ADDING_MEMBERS
+
 
 
 async def list_members(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    print("УЧАСТНИКИ ГРУППЫ:", context.user_data.get('team_members'))
-    await update.message.reply_text(context.user_data['team_members'])
+    text = update.message.text.lower()
+    context.user_data['choice'] = text
+    # team_members = update.message.parse_entities(types=["user"])
+    # print('team_members', team_members)
+    # context.user_data[ADDING_MEMBERS] = team_members
+    # print(context.user_data[ADDING_MEMBERS])
+    reply_text = "Вот кого удалось добавить:" + context.user_data['team_members']
+    # await update.message.reply_text(reply_text, ", ".join(context.user_data.get('team_members').values()))
+    await context.bot.send_message(
+        text=reply_text,
+        chat_id=update.effective_message.chat_id
+    )
     # chat_id=update.effective_chat.id,
-    return CHOOSING
+    return CHOOSING_TO_DO
 
 
-async def shuffling_members(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def shuffle_members(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="Ничего не понятно, но очень интересно"
--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import logging
 from dotenv import load_dotenv
 import os
+from typing import Dict
+from telegram import __version__ as TG_VER
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import (
+        ApplicationBuilder,
+        ConversationHandler,
+        CommandHandler,
+        MessageHandler,
+        filters,
+        PicklePersistence,
+    )
+
+from commands import (
+    start,
+    help_command,
+    create_team,
+    add_members,
+    list_members,
+    shuffle_members,
+    unknown
+)
 
 load_dotenv()
 
-from typing import Dict
-
-from telegram import __version__ as TG_VER
 
 try:
     from telegram import __version_info__
@@ -20,28 +38,18 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
 
-from telegram import Update, ReplyKeyboardMarkup
-from telegram.ext import (
-        ApplicationBuilder,
-        ConversationHandler,
-        CommandHandler,
-        MessageHandler,
-        filters,
-        PicklePersistence,
-    )
-from commands import start, help_command, create_team, list_members, unknown
-
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-CREATING_TEAM, LISTING_MEMBERS, HELP, CHOOSING, SHUFFLING, = range(5)
+CREATING_TEAM, ADDING_MEMBERS, LISTING_MEMBERS, HELP, CHOOSING, SHUFFLING, CHOOSING_TO_DO, = range(7)
 
-if __name__ == '__main__':
-    #persistence = PicklePersistence(filepath="conversationbot")
-    application = ApplicationBuilder().token(os.environ.get("TOKEN")).build()
 
+def main() -> None:
+
+    persistence = PicklePersistence(filepath="botdata")
+    application = ApplicationBuilder().token(os.environ.get("TOKEN")).persistence(persistence).build()
 
     '''Handler Declarations'''
 
@@ -50,15 +58,15 @@ if __name__ == '__main__':
         states={
             CHOOSING: [
                 MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Создать группу$")),
+                    filters.Regex("^Создать группу$"),
                     create_team
                 ),
                 MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Кто в группе$")),
+                    filters.Regex("^Кто в группе$"),
                     list_members
                 ),
                 MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Помощь$")),
+                    filters.Regex("^Помощь$"),
                     help_command
                 )
             ],
@@ -68,22 +76,28 @@ if __name__ == '__main__':
                     create_team
                 )
             ],
+            ADDING_MEMBERS: [
+                MessageHandler(
+                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Создать группу$")),
+                    add_members
+                )
+            ],
             LISTING_MEMBERS: [
                 MessageHandler(
                     filters.TEXT & ~(filters.COMMAND | filters.Regex("^Кто в группе$")),
-                    list_members,
+                    list_members
                 )
             ],
             SHUFFLING: [
                 MessageHandler(
                     filters.TEXT & ~(filters.COMMAND | filters.Regex("^Кто в группе$")),
-                    list_members,
+                    shuffle_members,
                 )
             ],
         },
         fallbacks=[MessageHandler(filters.Regex("^Done$"), start)],
         name="my_conversation",
-        persistent=False,
+        persistent=True,
     )
     help_handler = MessageHandler(filters.COMMAND, help_command)
 
@@ -95,3 +109,7 @@ if __name__ == '__main__':
     application.add_handler(unknown_handler)
 
     application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
