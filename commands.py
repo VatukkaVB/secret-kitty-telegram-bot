504630f73affb2608beee93c52386a22f7048d95
VatukkaVB <68278352+VatukkaVB@users.noreply.github.com>
2022-11-03 00:03:29 +0300

    chore: Tinker

--- commands.py
+++ commands.py
@@ -1,8 +1,12 @@
 from uuid import uuid4
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import Update, ReplyKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import ContextTypes
 
-CREATING_TEAM, ADDING_MEMBERS, LISTING_MEMBERS, HELP, CHOOSING, SHUFFLING, CHOOSING_TO_DO, = range(7)
+CREATING_TEAM, ADDING_MEMBERS, LISTING_MEMBERS, WAITING, CHOOSING, SHUFFLING, CHOOSING_TO_DO, = range(7)
+
+
+def show_members(user_data):
+    return " \n".join(user_data['team_members'].values())
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -15,72 +19,93 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     reply_keyboard = [
-        ["Создать группу", "Кто в группе"],
-        ["Помощь"],
+        ["Создать группу"], ["Я жду"]
     ]
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
     reply_text = ('Привет! Это бот для тайного санты имени трусов хэллоу китти. '
                   'Если хочешь создать новую группу, напиши или выбери кнопку "Создать группу". '
-                  'Если хочешь получить список людей в группе, напиши или выбери кнопку "Кто в группе". '
+                  'Если ты ждешь, пока тебя распределят, нажми "Я жду"'
                   )
-    if context.user_data:
-        del context.user_data
-    text = update.message.text.lower()
-    context.user_data['choice'] = text
+    del context.user_data
     await update.message.reply_text(reply_text, reply_markup=markup)
     return CHOOSING
 
 
+async def check_in(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Waiting user:", update.effective_message.chat_id)
+    if context.bot_data['users'] is None:
+        context.bot_data['users'] = [update.effective_message.chat_id]
+    else:
+        context.bot_data['users'] = context.bot_data['users'].append(update.effective_message.chat_id)
+
+
 async def create_team(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    # reply_text = "Введи список тегов тех, кого хочешь добавить к группе, через запятую:"
-    # await update.message.reply_text(reply_text)
-    await context.bot.send_message(
-        text="Введи список тегов тех, кого хочешь добавить к группе, через запятую:",
-        chat_id=update.effective_message.chat_id
-    )
+    # user = update.message.from_user
+    reply_text = "Введи список тегов тех, кого хочешь добавить к группе, через запятую:"
+    await update.message.reply_text(reply_text)
 
     return ADDING_MEMBERS
 
 
 async def add_members(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    team_members = update.message.parse_entities(types=["user"])
-    print(update.message.parse_entities(types=["user"]))
+    print("ADD_MEMBERS")
+    # user = update.message.from_user
+    team_members = update.message.parse_entities()
+    print(team_members)
     context.user_data['team_members'] = team_members
     print("context: ", context.user_data)
-    if context.user_data['team_members']:
-        return LISTING_MEMBERS
-    else:
-        await context.bot.send_message(
-            text="Что-то пошло не так!",
-            chat_id=update.effective_message.chat_id
-        )
-        return ADDING_MEMBERS
+    print("USER DATA:", (context.user_data['team_members'].values() is not None))
 
+    reply_keyboard = [
+        ["Показать!"],
+    ]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    reply_text = "Проверим, все ли указанные тобой теги верные"
+    await update.message.reply_text(reply_text, reply_markup=markup)
+
+    if context.user_data['team_members'].values() is not None:
+        return LISTING_MEMBERS
+
+    # else:
+    #     await context.bot.send_message(
+    #         text="Что-то пошло не так!",
+    #         chat_id=update.effective_message.chat_id
+    #     )
+    #     return ADDING_MEMBERS
 
 
 async def list_members(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    text = update.message.text.lower()
-    context.user_data['choice'] = text
-    # team_members = update.message.parse_entities(types=["user"])
-    # print('team_members', team_members)
-    # context.user_data[ADDING_MEMBERS] = team_members
-    # print(context.user_data[ADDING_MEMBERS])
-    reply_text = "Вот кого удалось добавить:" + context.user_data['team_members']
+    print("LIST_MEMBERS")
+    reply_keyboard = [
+        InlineKeyboardButton(text="Разослать сантам", callback_data=str(SHUFFLING)),
+        InlineKeyboardButton(text="Заново ввести группу", callback_data=str(ADDING_MEMBERS)),
+    ]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    reply_text = ('Вот кого удалось добавить: \n'
+                  + show_members(context.user_data) +
+                  '\n Все верно? Тогда жми "Разослать", чтобы санты узнали, кому они дарят подарки!'
+                  '\n Или ты кого-то забыла? Тогда придется начать с начала :(')
     # await update.message.reply_text(reply_text, ", ".join(context.user_data.get('team_members').values()))
-    await context.bot.send_message(
-        text=reply_text,
-        chat_id=update.effective_message.chat_id
-    )
-    # chat_id=update.effective_chat.id,
-    return CHOOSING_TO_DO
+    await update.message.reply_text(reply_text, reply_markup=markup)
+    return SHUFFLING
 
 
 async def shuffle_members(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Ничего не понятно, но очень интересно"
-    )
+    people_safe = context.user_data['team_members'].items()
+    people = people_safe
+    santa_s = people
+    pairs = {}
+    for santa in santa_s:
+        potential_recipients = people
+        potential_recipients.remove(santa)
+
+        recipient = choice(potential_recipients)
+        pairs[santa] = recipient
+
+        people.remove(recipient)
+
+        people = people + [santa]
 
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
